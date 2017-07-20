b96f8a37dd14a36b1d734d488898e56a96cdb7af
Hack Reactor Student <student@hackreactor.com>
2017-07-19 17:36:16 -0700

    working on adding state to app.jsx all prior tests passing

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,11 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
@@ -19,3 +19,37 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
+
+// {
+//   kind: 'youtube#searchResult',
+//   etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
+//   id: {
+//     kind: 'youtube#video',
+//     videoId: '4ZAEBxGipoA'
+//   },
+//   snippet: {
+//     publishedAt: '2015-08-02T20:52:58.000Z',
+//     channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
+//     title: 'React JS Tutorial for Beginners - 1 - Introduction',
+//     description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
+//     thumbnails: {
+//       default: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
+//         width: 120,
+//         height: 90
+//       },
+//       medium: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
+//         width: 320,
+//         height: 180
+//       },
+//       high: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
+//         width: 480,
+//         height: 360
+//       }
+//     },
+//     channelTitle: 'thenewboston',
+//     liveBroadcastContent: 'none'
+//   }
+// }
